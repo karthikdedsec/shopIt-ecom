ef414739bd8130166ad02369c13bcd8265e5778c
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-02-22 13:19:38 +0530

    add data seeder file

--- api/controllers/product.controllers.js
+++ api/controllers/product.controllers.js
@@ -1,5 +1,18 @@
+import Product from "../models/product.js";
+
+//create new product => /api/v1/products
 export const getProducts = async (req, res) => {
+  const products = await Product.find();
   res.status(200).json({
-    message: "products route",
+    products,
+  });
+};
+
+//create new product => /api/v1/admin/products
+export const newProduct = async (req, res) => {
+  const product = await Product.create(req.body);
+
+  res.status(200).json({
+    product,
   });
 };
--- api/routes/product.route.js
+++ api/routes/product.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getProducts } from "../controllers/product.controllers.js";
+import { getProducts, newProduct } from "../controllers/product.controllers.js";
 
 const router = express.Router();
 
 router.route("/products").get(getProducts);
+router.route("/admin/products").post(newProduct);
 
 export default router;
