6aac7f59fed16e0a748aac199b19b2f52955d220
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-02-22 11:16:35 +0530

    create products route and create product model

--- api/app.js
+++ api/app.js
@@ -1,7 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
+import productRoute from "./routes/product.route.js";
+import { connectDatabase } from "./config/dbConnect.js";
+
+dotenv.config({ path: "api/config/config.env" });
+
+//connect to db
+connectDatabase();
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log(`server running on port 3000`);
+app.use(express.json());
+app.use("/api/v1", productRoute);
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `server running on port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+  );
 });
