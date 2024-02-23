2ebbcb3fc83d2e5e1a34a1656f3051484b9a4d5f
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-02-23 12:04:37 +0530

    add pagination to the api

--- api/controllers/product.controllers.js
+++ api/controllers/product.controllers.js
@@ -5,12 +5,17 @@ import ErrorHandler from "../utils/errorHandler.js";
 
 //create new product => /api/v1/products
 export const getProducts = catchAsyncErrors(async (req, res) => {
+  const resPerPage = 4;
   const apiFilters = new APIFilters(Product, req.query).search().filters();
 
   let products = await apiFilters.query;
   let filteredProductsCount = products.length;
 
+  apiFilters.pagination(resPerPage);
+  products = await apiFilters.query.clone();
+
   res.status(200).json({
+    resPerPage,
     filteredProductsCount,
     products,
   });
--- api/utils/apiFilters.js
+++ api/utils/apiFilters.js
@@ -22,19 +22,28 @@ class APIFilters {
     let queryCopy = { ...this.queryStr };
 
     //remove field
-    const { keyword, ...rest } = queryCopy;
-
-    queryCopy = rest;
+    // const fieldsToRemove = ["keyword", "page"];
+    // fieldsToRemove.forEach((el) => delete queryCopy[el]);
+    const { keyword, page, ...rest } = queryCopy;
 
     //advance filter for price, ratings etc
 
-    let queryStr = JSON.stringify(queryCopy);
+    let queryStr = JSON.stringify(rest);
 
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
     console.log(queryStr);
 
     this.query = this.query.find(JSON.parse(queryStr));
 
+    return this;
+  }
+
+  //pagination
+  pagination(resPerPage) {
+    const currentPage = Number(this.queryStr.page) || 1;
+    const skip = resPerPage * (currentPage - 1);
+
+    this.query = this.query.limit(resPerPage).skip(skip);
     return this;
   }
 }
