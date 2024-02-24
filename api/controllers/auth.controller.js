88246f9033da87ba1d5bcc9ce34f89acdcde41fb
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-02-24 11:43:51 +0530

    create register and login user api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 import User from "../models/user.js";
+import ErrorHandler from "../utils/errorHandler.js";
 
+//register user => /api/v1/register
 export const registerUser = catchAsyncErrors(async (req, res, next) => {
   const { name, email, password } = req.body;
 
@@ -10,7 +12,40 @@ export const registerUser = catchAsyncErrors(async (req, res, next) => {
     password,
   });
 
+  const token = user.getJwtToken();
+
   res.status(201).json({
-    success: true,
+    token,
+  });
+});
+
+//login user => /api/v1/login
+export const loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter email & password", 400));
+  }
+
+  //find user in the database
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+  //check if password is correct
+
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+  const token = user.getJwtToken();
+
+  res.status(200).json({
+    token,
   });
 });
