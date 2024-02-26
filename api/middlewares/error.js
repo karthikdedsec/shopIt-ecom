8254ef62451699c7567ce766caa45c97d2e8d0fd
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-02-26 11:19:49 +0530

    handle duplicate key and jwt errors

--- api/middlewares/error.js
+++ api/middlewares/error.js
@@ -18,6 +18,23 @@ export default (err, req, res, next) => {
     error = new ErrorHandler(message, 400);
   }
 
+  //handle mongoose duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate: ${Object.keys(err.keyValue)} entered`;
+    error = new ErrorHandler(message, 400);
+  }
+
+  //handle wrong JWT error
+  if (err.name === "JsonWebTokenError") {
+    const message = `JSON web token is invalid.Try Again!!!`;
+    error = new ErrorHandler(message, 400);
+  }
+  //handle expired JWT error
+  if (err.name === "TokenExpiredError") {
+    const message = `JSON web token is Expired.Try Again!!!`;
+    error = new ErrorHandler(message, 400);
+  }
+
   if (process.env.NODE_ENV === "DEVELOPMENT") {
     res.status(error.statusCode).json({
       message: error.message,
