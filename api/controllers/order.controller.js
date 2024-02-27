3c921ee29bc2de9b9e60b3990d91830a0af724ee
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-02-27 12:37:30 +0530

    create the get all orders and update orders route

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -1,5 +1,6 @@
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 import Order from "../models/order.js";
+import Product from "../models/product.js";
 import ErrorHandler from "../utils/errorHandler.js";
 
 //create new order => /api/v1/orders/new
@@ -29,5 +30,81 @@ export const newOrder = catchAsyncErrors(async (req, res, next) => {
 
   res.status(200).json({
     order,
+  });
+});
+
+//get order details  => /api/v1/orders/:id
+export const getOrderDetails = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new ErrorHandler("Order not found with this id", 404));
+  }
+
+  res.status(200).json({
+    order,
+  });
+});
+
+//get current user orders  => /api/v1/me/orders
+export const myOrders = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.find({ user: req.user._id }).populate(
+    "user",
+    "name email"
+  );
+
+  if (!order) {
+    return next(new ErrorHandler("No orders found!", 404));
+  }
+
+  res.status(200).json({
+    order,
+  });
+});
+
+//get all orders - admin  => /api/v1/admin/orders
+export const allOrders = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.find();
+
+  if (!order) {
+    return next(new ErrorHandler("No orders found!", 404));
+  }
+
+  res.status(200).json({
+    order,
+  });
+});
+
+//update orders - admin  => /api/v1/admin/orders/:id
+export const updateOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new ErrorHandler("No orders found with this id!", 404));
+  }
+
+  if (order?.orderStatus === "Delivered") {
+    return next(new ErrorHandler("Already delivered this order", 400));
+  }
+
+  //update product stock
+  order?.orderItems?.forEach(async (item) => {
+    const product = await Product.findById(item?.product?.toString());
+
+    if (!product) {
+      return next(new ErrorHandler("No product found with this id!", 404));
+    }
+
+    product.stock = product.stock - item.quantity;
+    await product.save({ validateBeforeSave: false });
+  });
+
+  order.orderStatus = req.body.status;
+  order.deliveredAt = Date.now();
+
+  await order.save();
+
+  res.status(200).json({
+    success: true,
   });
 });
--- api/routes/order.route.js
+++ api/routes/order.route.js
@@ -1,9 +1,25 @@
 import express from "express";
-import { isAuthenticatedUser } from "../middlewares/auth.js";
-import { newOrder } from "../controllers/order.controller.js";
+import { authorizeRoles, isAuthenticatedUser } from "../middlewares/auth.js";
+import {
+  allOrders,
+  getOrderDetails,
+  myOrders,
+  newOrder,
+  updateOrder,
+} from "../controllers/order.controller.js";
 
 const router = express.Router();
 
 router.route("/orders/new").post(isAuthenticatedUser, newOrder);
+router.route("/orders/:id").get(isAuthenticatedUser, getOrderDetails);
+router.route("/me/orders").get(isAuthenticatedUser, myOrders);
+
+router
+  .route("/admin/orders")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), allOrders);
+
+router
+  .route("/admin/orders/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder);
 
 export default router;
