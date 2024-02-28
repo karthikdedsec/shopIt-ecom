6f370383cae84a510719cb4395a04b21899dd7dc
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-02-28 16:17:29 +0530

    complete the product details page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,20 @@ import Home from "./components/Home";
 
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
+import { Toaster } from "react-hot-toast";
+import ProductDetails from "./components/product/ProductDetails";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Toaster position="top-center" />
         <Header />
 
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<ProductDetails />} />
           </Routes>
         </div>
 
