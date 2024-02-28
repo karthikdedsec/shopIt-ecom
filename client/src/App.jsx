de1f4a802648f8fb73aa9117402b6c6a8b899e56
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-02-28 11:18:09 +0530

    add react helmet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./components/Home";
 
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
