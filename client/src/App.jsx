e0b138c5d3e8e7cdc5f635d064ecb20f64687ac2
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-02-28 11:01:38 +0530

    create and add header and footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,15 @@
+import "./App.css";
+
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
+
 function App() {
-  return <div>Hello There</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
