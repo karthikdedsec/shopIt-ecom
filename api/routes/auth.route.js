9063daf95c14a751e821712332491c74f41cd9c6
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-03-01 14:20:14 +0530

    handle all authentication integration

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -18,7 +18,7 @@ import { authorizeRoles, isAuthenticatedUser } from "../middlewares/auth.js";
 const router = express.Router();
 
 router.route("/register").post(registerUser);
-router.route("/login").get(loginUser);
+router.route("/login").post(loginUser);
 router.route("/logout").get(logout);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
