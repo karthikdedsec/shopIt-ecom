4489435cc9158682c120d6c911c51c5eeb5602fa
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-03-02 09:27:59 +0530

    create user profile ui section

---
+++ client/src/components/layout/UserLayout.jsx
@@ -0,0 +1,20 @@
+import SidebarMenu from "./SidebarMenu";
+
+const UserLayout = ({ children }) => {
+  return (
+    <div>
+      <div className="mt-2 mb-4 p-y">
+        <h2 className="text-center fw-bolder">User Settings</h2>
+      </div>
+      <div className="container">
+        <div className="row justify-content-around">
+          <div className="col-12 col-lg-3">
+            <SidebarMenu />
+          </div>
+          <div className="col-12 col-lg-8 user-dashboard">{children}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default UserLayout;
