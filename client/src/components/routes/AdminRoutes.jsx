4ffb57958885799378e5b77742451f174e3624c2
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-03-10 10:05:56 +0530

    create admin route and dashboard layout

---
+++ client/src/components/routes/AdminRoutes.jsx
@@ -0,0 +1,19 @@
+import { Route } from "react-router-dom";
+import ProtectedRoute from "../auth/ProtectedRoute";
+import Dashboard from "../admin/Dashboard";
+
+const AdminRoutes = () => {
+  return (
+    <>
+      <Route
+        path="/admin/dashboard"
+        element={
+          <ProtectedRoute admin={true}>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+    </>
+  );
+};
+export default AdminRoutes;
