d2933b825d37ac538289336b4453574769abdaf2
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-03-13 09:36:42 +0530

    display all orders

---
+++ client/src/components/admin/ListOrders.jsx
@@ -0,0 +1,107 @@
+import { useEffect } from "react";
+import toast from "react-hot-toast";
+import Loader from "../layout/Loader";
+import { MDBDataTable } from "mdbreact";
+import { Link } from "react-router-dom";
+import MetaData from "../layout/MetaData";
+import AdminLayout from "../layout/AdminLayout";
+import { useGetAdminOrdersQuery } from "../../redux/api/orderApi";
+
+const ListOrders = () => {
+  const { data, isLoading, error } = useGetAdminOrdersQuery();
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error?.data?.message);
+    }
+    // if (deleteError) {
+    //   toast.error(deleteError?.data?.message);
+    // }
+    // if (isSuccess) {
+    //   toast.success("Product deleted");
+    // }
+  }, [error]);
+
+  //   const deleteProductHandler = (id) => {
+  //     deleteProduct(id);
+  //   };
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  const setOrders = () => {
+    const orders = {
+      columns: [
+        {
+          label: "ID",
+          field: "id",
+          sort: "asc",
+        },
+        {
+          label: "Payment Status",
+          field: "paymentStatus",
+          sort: "asc",
+        },
+        {
+          label: "Order Status",
+          field: "orderStatus",
+          sort: "asc",
+        },
+
+        {
+          label: "Actions",
+          field: "actions",
+          sort: "asc",
+        },
+      ],
+      rows: [],
+    };
+
+    data?.order?.forEach((ordr) => {
+      orders.rows.push({
+        id: ordr?._id,
+        paymentStatus: ordr?.paymentInfo?.status?.toUpperCase(),
+        orderStatus: ordr?.orderStatus,
+        actions: (
+          <>
+            <Link
+              to={`/admin/orders/${ordr?._id}`}
+              className="btn btn-outline-primary"
+            >
+              <i className="fa fa-pencil"></i>
+            </Link>
+
+            <button
+              className="btn btn-outline-danger ms-2"
+              //   onClick={() => deleteProductHandler(product?._id)}
+              //   disabled={isDeleteLoading}
+            >
+              <i className="fa fa-trash"></i>
+            </button>
+          </>
+        ),
+      });
+    });
+
+    return orders;
+  };
+
+  return (
+    <AdminLayout>
+      <MetaData title={"All products"} />
+      <div>
+        <h1 className="my-5">{data?.order?.length} Orders</h1>
+
+        <MDBDataTable
+          data={setOrders()}
+          className="px-3"
+          bordered
+          striped
+          hover
+        />
+      </div>
+    </AdminLayout>
+  );
+};
+export default ListOrders;
--- client/src/components/routes/AdminRoutes.jsx
+++ client/src/components/routes/AdminRoutes.jsx
@@ -5,6 +5,7 @@ import ListProducts from "../admin/ListProducts";
 import NewProduct from "../admin/NewProduct";
 import UpdateProduct from "../admin/UpdateProduct";
 import UploadImages from "../admin/UploadImages";
+import ListOrders from "../admin/ListOrders";
 
 const AdminRoutes = () => {
   return (
@@ -46,6 +47,15 @@ const AdminRoutes = () => {
         element={
           <ProtectedRoute admin={true}>
             <UploadImages />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/admin/orders"
+        element={
+          <ProtectedRoute admin={true}>
+            <ListOrders />
           </ProtectedRoute>
         }
       />
